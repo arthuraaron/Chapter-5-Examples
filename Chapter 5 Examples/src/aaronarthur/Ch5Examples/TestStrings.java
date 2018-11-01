6e591ae5cf073ddb1654b94279ce8e9bbcb4a8ee
aaron.d.arthur <aaron.d.arthur@CLC-LAB209-10.TCSD.tooelesd.org>
2018-11-01 12:33:08 -0600

    11/1

--- Chapter 5 Examples/src/aaronarthur/Ch5Examples/TestStrings.java
+++ Chapter 5 Examples/src/aaronarthur/Ch5Examples/TestStrings.java
@@ -1,10 +1,15 @@
 package aaronarthur.Ch5Examples;
 
+import java.util.Scanner;
+
 public class TestStrings {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+		String message = "Welcome to Java";
+		
+		
 	}
 
 }
